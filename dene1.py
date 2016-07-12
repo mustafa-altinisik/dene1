e16a4c6f1732dfd7fa1b13820a227d18b0ae7c36
Mustafa Asım Altınışık <mustafa@altinisik.net>
2016-07-12 13:46:47 +0300

    English language support

--- dene1.py
+++ dene1.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
 pencere = Tk()
-etiket = Label(text = "Abim istedi,yaptım.\nHello World!", fg="green")
+etiket = Label(text = "My brother wanted,and I did.\nHello World!", fg="green")
 etiket.pack()
 mainloop()
 #çalıştı 11.07.16
